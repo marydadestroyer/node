572d432f1fd3712cc81444755c29384be637ce98
Mary Harrell <harrell_mary@columbusstate.edu>
2019-10-08 22:57:22 +0000

    trying node

---
+++ app.js
@@ -0,0 +1,19 @@
+// clall express
+
+const express = require("express");  //saying we require it
+const app = express();   //this variable is going to call express
+const port = process.env.PORT || 3000;   //andre  for maintainablility
+
+// single slash is home page
+app.get('/', function(req, res){
+res. send("hello clair");
+
+});
+
+app.get('/about',function(req, res){
+    res.send("<h1>same hoes</h1>");
+})
+
+app.listen(port, funtion(){
+
+});
