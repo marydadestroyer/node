9b0539e347ef0684dfb03020443260eb668843b5
Mary Harrell <harrell_mary@columbusstate.edu>
2019-10-15 23:06:12 +0000

    stuff1

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 // clall express
 var http = require('http');
 var path = require('path');
+var bodyParser = require("body-parser");
 const express = require("express");  //saying we require it
 const app = express();   //this variable is going to call express
 const port = process.env.PORT || 3000;   //andre  for maintainablility
 
 
+
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ encoded: true}));
+var task = ["clean", "laundry"];
 
 // single slash is home page
 app.get('/', function(req, res){
@@ -15,8 +21,10 @@ res.render('index');
 
 });
 
-app.get('/about',function(req, res){
-    res.send("<h1>same hoes</h1>");
+app.post('/addtask',function(req, res){
+    var newTask = req.body.newtask;
+    task.push(newTask);
+    res.redirect('/');
 });
 
 http.createServer(app).listen(port, function(){
