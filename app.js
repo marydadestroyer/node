4d548ea5bf63bf5aafc79dfcc86f48bee4e2ab57
Mary Harrell <harrell_mary@columbusstate.edu>
2019-10-20 19:40:35 +0000

    partial work from class

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var bodyParser = require("body-parser");
 var express = require("express");  //saying we require it
 var app = express();   //this variable is going to call express
 var port = process.env.PORT || 3000;   //andre  for maintainablility
+var complete;
+
 
 
 
@@ -24,6 +26,23 @@ res.render('index', {task:task});
 app.post('/addtask',function(req, res){
     var newTask = req.body.newtask;
     task.push(newTask);
+    res.redirect('/');
+});
+
+app.post('/removetask',function(req, res){
+    var completeTask = req.body.check;
+    //task.push(newTask);
+    if(){(typeOf completeTask ==='string')
+        complete.push(completeTask);
+        task.splice(task.indexOf(completeTask),1);
+    
+    }elseIf{(typeOf completeTask === "object")
+        for()var i = 0; i < completeTask.length; i++){
+          complete.push(completeTask[i]);
+        task.splice(task.indexOf(completeTask[i]),1);  
+        }
+    
+    }
     res.redirect('/');
 });
 
